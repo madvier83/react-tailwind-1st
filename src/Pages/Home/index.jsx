25cd7e2a1b233672aa59192678eefe22b1ce8046
madvier83 <madvier83@gmail.com>
2022-06-21 05:13:00 +0700

    Update homepage

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -2,14 +2,16 @@ const Home = () => {
   return (
     <>
       {/* Wrapper */}
-      <div className="w-full h-full min-h-screen bg-slate-900 text-slate-400">
-        <h1>Homepage</h1>
-        <div className="flex">
-          {/* <img
-          src={process.env.PUBLIC_URL + "/images/serika.jpg"}
-          alt="logo"
-          className="rounded"
-        /> */}
+      <div className="w-full h-full min-h-screen bg-slate-900 text-slate-300">
+        <div className="flex flex-col items-center justify-end h-[60vh] px-12 md:px-48">
+          <h1 className="text-4xl font-bold">React + Tailwind Code Examples</h1>
+          <p className="mt-8 text-justify text-slate-400">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Dignissimos obcaecati quia libero inventore at provident modi itaque
+            odio minima? Beatae temporibus sapiente optio totam omnis alias
+            fugiat porro corporis voluptas, delectus ducimus asperiores facere
+            unde? Assumenda, ipsa!
+          </p>
         </div>
       </div>
     </>
